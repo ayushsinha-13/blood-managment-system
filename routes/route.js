c4f0fe7ff55179273aeb7c52c234bcc5da39d7b8
Ayush Sinha <ayushsinhajsr20@gmail.com>
2022-05-16 18:10:11 +0530

    Completed Frontend

--- routes/route.js
+++ routes/route.js
@@ -1,45 +1,62 @@
 const express = require('express')
+const  https = require('https')
 
 const app = express()
 
 // Template Engine
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 
 app.route('/')
     .get((req,res)=> res.render("Home"))
-    .post((req,res)=> {
-        res.redirect('/status')
-    })
 
 app.route('/status')
-    .get((req,res)=> res.render('Status'))    
+    .get((req,res) => res.render('Status'))
 
 app.route('/admin-login')
-    .get((req,res)=> res.render('Admin-Login')) 
+    .get((req,res)=> res.render('Admin-Login'))
     .post((req,res)=> res.redirect('/admin-hospital'))
 
 
 // ADMIN PANEL
  app.route('/admin-hospital')
-    .get((req,res) => res.render('Admin-Panel-Hospital'))    
+    .get((req,res) => res.render('Admin-Panel-Hospital'))
  app.route('/admin-donor')
-    .get((req,res) => res.render('Admin-Panel-Donor'))    
+    .get((req,res) => res.render('Admin-Panel-Donor'))
  app.route('/admin-recipient')
-    .get((req,res) => res.render('Admin-Panel-Recipient'))    
+    .get((req,res) => res.render('Admin-Panel-Recipient'))
  app.route('/admin-appointment')
-    .get((req,res) => res.render('Admin-Panel-Appointment'))    
+    .get((req,res) => res.render('Admin-Panel-Appointment'))
 
-    
+
 app.route('/confirmation')
     .get((req,res)=> res.render('Confirmation'))
 
-app.route('/donor')
-    .get((req,res)=> res.render('Donor-Hospital-List'))
-    .post((req,res)=> res.redirect('/donor'))
+// DONOR
 
-app.route('/check')
-    .get((req,res)=> res.render('Donor-Hospital-Form'))
+app.route('/donor')
+  .get((req,res)=> res.redirect('/'))
+  .post((req,res)=>{
+        const pincode = req.body.pincode
+        res.render('Donor-Hospital-List', {pincode: pincode})
+  })
+app.route('/donor-form')
+  .get((req,res)=> res.render('Donor-Hospital-Form'))
+  .post((req,res)=> res.render('Confirmation'))
+
+// RECIPIENT
+
+app.route('/recipient')
+  .get((req,res)=> res.redirect('/'))
+  .post((req,res)=>{
+    const blood = req.body.bloodGroup
+    res.render('Recipient-Hospital-List', {blood: blood})
+  })
+app.route('/recipient-form')
+.get((req,res)=> res.render('Recipient-Hospital-Form'))
+.post((req,res)=> res.render('Confirmation'))
+
 
 // XXXXXXXXXXXXXXXXXXXXX
 // DON'T TOUCH THE CODE BELOW THIS
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const app = express()
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: false}))
 
 const routes = require('./routes/route')
 app.use(routes)
